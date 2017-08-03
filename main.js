26069d2703052a788daa49cc979a43bd7f1f79d9
Devon Moubry <devon@moubry.com>
2017-08-03 15:22:41 -0500

    put value in output on click

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 console.log('foo bar');
 
 let button = document.getElementsByClassName('cal');
+let output = document.getElementById('output-area');
 for (var i = 0; i < button.length; i++) {
   console.log(button[i]);
+  button[i].addEventListener('click', function () {
+    output.innerHTML = this.value;
+  });
 }
