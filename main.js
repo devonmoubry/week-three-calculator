23391ceefa51727b9989cf4219c3ffccebb7221e
Devon Moubry <devon@moubry.com>
2017-08-10 11:52:44 -0500

    add function to calculator

--- main.js
+++ main.js
@@ -1,10 +1,25 @@
-console.log('foo bar');
+const maximumLength = 9;
+let buttons = document.querySelectorAll('.action');
+let outputScreen = document.querySelector('#output-screen');
+let clearButton = document.querySelector('#clear-button');
+let equalButton = document.querySelector('#equal-button');
 
-let button = document.getElementsByClassName('cal');
-let output = document.getElementById('output-area');
-for (var i = 0; i < button.length; i++) {
-  console.log(button[i]);
-  button[i].addEventListener('click', function () {
-    output.innerHTML = this.value;
-  });
-}
+clearButton.addEventListener('click', function() {
+  outputScreen.innerHTML = '';
+}); // clearButton.addEventListener()
+
+buttons.forEach(function(button) {
+  let buttonValue = button.value;
+
+  button.addEventListener('click', function() {
+
+    if (outputScreen.textContent.length < maximumLength) {
+      outputScreen.textContent += buttonValue;
+    } // if
+  }) // button.eventListener()
+}); // buttons.forEach()
+
+equalButton.addEventListener('click', function() {
+  let answer = eval(outputScreen.textContent);
+  outputScreen.innerHTML = answer;
+}); // equalButton.addEventListener()
